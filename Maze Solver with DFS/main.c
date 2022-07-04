53e3c17c576a2aa16866ea35bcf34608adac48e3
Emircan KİREZ - Ch3rry <73709289+Emircan-Kirez@users.noreply.github.com>
2022-07-04 15:01:36 +0300

    Update main.c

--- Maze Solver with DFS/main.c
+++ Maze Solver with DFS/main.c
@@ -1,9 +1,9 @@
-/* Emircan KÝREZ - Ch3rry */
+/* Emircan KÄ°REZ - Ch3rry */
 /* Last Update: 02/06/2022 */
 /*
 ----------------------- INFORMATION ---------------------------
-• It is recommended to read the README.md file before proceeding to the project.
-• For any questions and suggestions related to the project, via the e-mail address "emircan200123@hotmail.com", you can contact me by
+â€¢ It is recommended to read the README.md file before proceeding to the project.
+â€¢ For any questions and suggestions related to the project, via the e-mail address "emircan200123@hotmail.com", you can contact me by
 typing "Github | C-EN / Maze Solver with DFS" to the subject line 
 */
 #include <stdio.h>
